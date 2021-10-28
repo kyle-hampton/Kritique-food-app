f6399b261166eaab372080a6d6d8341d0753cf76
kyle-hampton <kyledr215@gmail.com>
2021-10-28 12:42:36 -0400

    class AddFoodExpantionToFoods < ActiveRecord::Migration[5.2] updated

--- db/migrate/20181213225212_add_food_expantion_to_foods.rb
+++ db/migrate/20181213225212_add_food_expantion_to_foods.rb
@@ -2,6 +2,6 @@ class AddFoodExpantionToFoods < ActiveRecord::Migration[5.2]
   def change
     add_column :foods, :food_description, :string
     add_column :foods, :food_review, :string
-    add_column :foods, :food_original_score, :interger
+    add_column :foods, :food_original_score, :integer
   end
 end
