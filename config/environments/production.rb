d1a949eed1dd578bdbbe162b6b8e5e3e8899bd4b
kyle-hampton <kyledr215@gmail.com>
2021-10-28 10:47:54 -0400

    asset precompile added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
